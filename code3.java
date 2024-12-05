2a49929a9490566a412208fd22e986c4c7c0410e
Manasvi Singh <shivamanasvi5@gmail.com>
2024-12-05 19:04:08 +0530

    Add files via upload

---
+++ code3.java
@@ -0,0 +1,10 @@
+public class code3 
+{//(operators)
+public static void main(String[] args) 
+{
+System.out.println(10%2);//0
+System.out.println(10%3);//1
+System.out.println(8%4);//0
+System.out.println(4%8);//4
+}    
+}
