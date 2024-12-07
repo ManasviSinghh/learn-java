d1a29aa2c40ace0eec4da6b8e3d4bb8554f03631
Manasvi Singh <shivamanasvi5@gmail.com>
2024-12-07 17:55:43 +0530

    Add files via upload

---
+++ code4.java
@@ -0,0 +1,11 @@
+public class code4 
+{//(relational operators)
+public static void main(String[] args) 
+{
+System.out.println(10==10);//T 
+System.out.println(10<4);//F
+System.out.println(10>4);//T
+System.out.println(10<=10);//T
+System.out.println(10>=14);//F   
+}    
+}
