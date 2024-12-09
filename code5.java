a25295fe1737a9acf53b69c391e3d656ef36bbb7
Manasvi Singh <shivamanasvi5@gmail.com>
2024-12-09 17:46:23 +0530

    Add files via upload

---
+++ code5.java
@@ -0,0 +1,15 @@
+public class code5 
+{//else if ladder
+public static void main(String[] args) 
+{
+int age=18;
+if (age>=10)
+{
+    System.out.println("eligible for license");
+}   
+else
+{
+    System.out.println("calm down");
+}
+}    
+}
