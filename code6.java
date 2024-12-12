62accfc2e2b856a7c5ffbccfcd5711c6a06f8c73
Manasvi Singh <shivamanasvi5@gmail.com>
2024-12-12 21:17:43 +0530

    Add files via upload

---
+++ code6.java
@@ -0,0 +1,21 @@
+
+
+public class code6 
+{
+public static void main(String[] args) 
+{
+int age =18;
+if(age==18)
+{
+    System.out.println("you have grown be responsible");
+}   
+else if (age<=18)
+{
+    System.out.println("you are not more a teen ager");
+} 
+else
+{
+    System.out.println("something");
+}
+}    
+}
